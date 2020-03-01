b180ace8b01b7b13adff33a8e24ab7d6b8f97cd0
mario2598 <43936591+mario2598@users.noreply.github.com>
2020-02-29 22:38:21 -0600

    dfvd

---
+++ meetSiripiqui/resources/views/layout/navBar.blade.php
@@ -0,0 +1,48 @@
+<header style=''>
+			<!-- Start Top Header -->
+			<div id="sticker" class="header-area">
+				<div class="container">
+					<!-- Navigation -->
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<!-- Brand -->
+							<a class="navbar-brand page-scroll" href="#page-top"><img src="{{asset("assets/img/logo2.png")}}" alt=""></a>
+							<a class="navbar-brand page-scroll sticky-logo" href="#page-top"><img src="{{asset("assets/img/logo.png")}}" alt=""></a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse main-menu bs-example-navbar-collapse-1" id="navbar-example">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active">
+									<a class="page-scroll" href="#home">Home</a>
+								</li>
+								<li>
+									<a class="page-scroll" href="#about">About</a>
+								</li>		              	
+								<li>
+									<a class="page-scroll" href="#portfolio">Portfolio</a>
+								</li>
+								<li>
+									<a class="page-scroll" href="#blog">Blog</a>
+								</li>		              	              	              	
+								<li>
+									<a class="page-scroll" href="#contact">Contact</a>
+								</li>		              			              
+							</ul>
+						</div>
+						<!-- navbar-collapse -->
+					</nav>
+					<!-- END: Navigation -->
+				</div>
+			</div>
+			<!-- End Top Header -->
+			<!-- Start Bottom Header -->
+			
+			<!-- Start Bottom Header -->
+		
