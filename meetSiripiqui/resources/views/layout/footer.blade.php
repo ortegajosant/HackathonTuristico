0ecb2700f93ca5fe749997fcc4b992dfdcd09380
mario2598 <43936591+mario2598@users.noreply.github.com>
2020-02-29 23:30:55 -0600

    sdsd

---
+++ meetSiripiqui/resources/views/layout/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="footer-area text-center">
+		   <div class="container">
+			   <div class="row">
+				  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="footer-icons text-center">
+							<ul>
+								<li>
+									<a href="#"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-rss"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-youtube"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</li>
+							</ul>
+						</div>
+						<div class="copyright text-center">
+							
+						</div>
+				   </div>
+			   </div>
+		   </div>
+		</div>
